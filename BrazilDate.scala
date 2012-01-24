46381297a110a98d05842c1a6881e6df35c3814f
Thiago Caiubi <thiago.caiubi@gmail.com>
2012-01-23 22:35:42 -0200

    Prints long date in Brazil format

---
+++ BrazilDate.scala
@@ -0,0 +1,12 @@
+import java.util.{Date, Locale}
+import java.text.DateFormat
+import java.text.DateFormat._
+
+object BrazilDate {
+	
+	def main(args: Array[String]) {
+		val now = new Date
+		val dateFormat = getDateInstance(LONG, new Locale("pt", "BR"))
+		println(dateFormat format now)
+	}
+}
