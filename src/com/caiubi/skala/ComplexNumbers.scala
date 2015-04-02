4518806aea5e4c9aa5b68b502f73d1eb368e3daa
Thiago Caiubi <thiago.caiubi@gmail.com>
2015-04-02 09:20:25 -0300

    Delete old stuff

--- src/com/caiubi/skala/ComplexNumbers.scala
+++
@@ -1,9 +0,0 @@
-package com.caiubi.skala;
-
-object ComplexNumbers {
-	
-	def main(args: Array[String]) {
-		val c = new Complex(1.2, 3.4)
-		println("imaginary part: " + c)
-	}
-}
--- src/com/caiubi/skala/HelloWorld.scala
+++
@@ -1,8 +0,0 @@
-package com.caiubi.skala;
-
-object HelloWorld {
-	
-	def main(args: Array[String]) {
-		println("Hello World!")
-	}
-}
